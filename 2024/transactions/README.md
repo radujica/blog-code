94e38469a5d10580fad5a690fb59cb98accbddaf
Radu <radu.jica+Code@gmail.com>
2024-08-29 09:45:04 +0200

    Add transactions code (#3)

---
+++ 2024/transactions/README.md
@@ -0,0 +1,10 @@
+# Setup
+
+    conda create -n transactions python
+    conda activate transactions && pip install pytest
+
+
+# Run
+
+    conda run --name transactions pytest
+    conda run --name transactions python run.py
---
+++ 2024/transactions/tests/pipeline.py
@@ -0,0 +1,42 @@
+from decorator import rollback, transaction
+
+# the inner funcs are purely to make mocking easier in the tests
+
+def inner_revert_write(name: str):
+    print(f"reverting write of {name}")
+
+
+def revert_write(name: str):
+    inner_revert_write(name)
+
+
+def inner_revert_log():
+    print("reverting log")
+
+
+def revert_log():
+    inner_revert_log()
+
+
+def inner_write(name: str):
+    print(f"writing {name}")
+
+
+@rollback(rollback_func=revert_write)
+def write(name: str):
+    inner_write(name)
+
+
+def inner_log():
+    print("logging")
+
+
+@rollback(rollback_func=revert_log)
+def log():
+    inner_log()
+
+
+@transaction
+def run_two_things(name: str):
+    write(name)
+    log()
